51a46c41607ebc1e25561a2049ea15da5108c016
Subodh Uttam Muneshwar <164660159+SubodhMuneshwar@users.noreply.github.com>
2024-07-25 17:52:30 +0000

    datatypes

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -2,10 +2,23 @@
 
 // alert(3+5) //this is NOT ALLOWED as we are using NODEJS and not BROWSER
 
-console.log(3+
-    3); // X code readability should be high
+console.log(3+3); // X code readability should be high
 
 let name="Motya"
 let age=21
 let isLoggedIn= true
 
+
+
+//number ==> 2 to power 53 {range}
+// bigint [for a very large number. eg. for trading or stock market or for big website like reddit instagram etc]
+//string ==> use in double quotes ""
+//boolean ==> true or false 
+//null [it is a stand alone value/ representation of an empty value]
+//undefined [no value assign]
+//symbol ==> unique
+
+
+// OBJECT
+
+console.log(typeof null) //null is an object and undefined is undefined
