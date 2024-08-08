aaa1ce1b908d43b305b5c3b24ab30aaa73876a80
Subodh Uttam Muneshwar <164660159+SubodhMuneshwar@users.noreply.github.com>
2024-08-08 13:23:05 +0000

    conversions of datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,70 @@
+// console.log("1:");
+
+// let score1 = 33;
+
+// // console.log(typeof(score)); //we can also write in this format
+// console.log(typeof score1);
+
+// console.log("2:");
+// let score2 = "33"
+// let valueInNumber = Number(score2);
+// console.log(typeof valueInNumber);
+
+// console.log("3:");
+// let score3 = "33subodh"; //NAN
+// let valueInNumber2 = Number(score3)
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+// console.log("4:");
+// let score4 = null
+// let valueInNumber3 = Number(score4);
+// console.log(valueInNumber3);
+// console.log(typeof score4);
+// console.log(typeof valueInNumber3);
+
+// console.log("5:");
+// let score5 = undefined;
+// let valueInNumber4 = Number(score5);
+// console.log(valueInNumber4);
+// console.log(typeof score5);
+// console.log(typeof valueInNumber4);
+
+// console.log("6:");
+// let score6 = true;
+// let valueInNumber5 = Number(score6);
+// console.log(valueInNumber5);
+// console.log(typeof score6);
+// console.log(typeof valueInNumber5);
+
+// console.log("7:");
+// let score7 = "Subodh"
+// let valueInNumber6 = Number(score7);
+// console.log(valueInNumber6);
+// console.log(typeof score7);
+// console.log(typeof valueInNumber6);
+
+/* "33" => 33
+ "33subodh" => NaN
+ true => 1 ; false => 0
+*/
+
+let isLoggedIn = 1 //=> true
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+let isLoggedIn1 = "subodh"  //=> true 
+let booleanisLoggedIn1 = Boolean(isLoggedIn1)
+console.log(booleanisLoggedIn1);
+
+let isLoggedIn2 = "" //=> false
+let booleanisLoggedIn2 = Boolean(isLoggedIn2)
+console.log(booleanisLoggedIn2);
+
+
+let someNumber = 33
+let StringNumber = String(someNumber);
+console.log(typeof StringNumber);
+console.log(someNumber);
+
+
