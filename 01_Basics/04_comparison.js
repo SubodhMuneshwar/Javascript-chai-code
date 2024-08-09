0212ceb3ed14353c92d9ce37779b2cd39d6d6825
Subodh Uttam Muneshwar <164660159+SubodhMuneshwar@users.noreply.github.com>
2024-08-09 19:17:12 +0000

    comparison done

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(2>=1);
+// console.log(2!=1);
+// console.log(2==1);
+
+// console.log("2"==1);
+
+/* TYPE SCRIPT DOESN'T ALLOW TO COMPARE 2 DIFFERENT DATATYPES*/
+
+// console.log(true > 2);
+// console.log(null > 2);
+// console.log(null == 0);
+// console.log(null >= 0); //value conversion problem
+
+/* COMPARISON COVERT NULL TO A NUNMBER TREATING IT AS 0. 
+HENCE  null >=0 is true and null>0 is false*/
+
+// console.log(undefined == 0);   // flase
+// console.log(undefined > 0);   // flase
+// console.log(undefined < 0);  // flase
+
+/* === (strict check which checks value and datatype) */
+
+console.log("2" === 2);
+
+
+
+
+
