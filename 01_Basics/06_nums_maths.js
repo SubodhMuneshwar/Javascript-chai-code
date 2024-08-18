da22c665d37c71970d5324c5e6234db9a05db8ae
Subodh Uttam Muneshwar <164660159+SubodhMuneshwar@users.noreply.github.com>
2024-08-18 18:29:46 +0000

    done math and num

---
+++ 01_Basics/06_nums_maths.js
@@ -0,0 +1,40 @@
+let score=100
+console.log(score);
+
+let balance = new Number(7355.9987);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(4));
+
+let newNo = 20000000000;
+// console.log(newNo.toLocaleString('en-IN'));
+
+// console.log(balance.isSafeInteger(););
+
+
+// +++++++++++++++++++++++++++MATHSS++++++++++++++++++++//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(25,2,1,44));
+
+  // console.log((Math.random()*10) +1);
+
+//add 1 to avoid the value 0 
+//math.random gives us value between 0 and 1 
+
+  // console.log(Math.floor(Math.random()*10) +1);
+
+const min= 10
+const max= 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+ 
